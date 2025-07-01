91601fce6e71b9f752f123fa6dcfa9def5151758
whitee00 <138634064+whitee00@users.noreply.github.com>
2025-07-01 20:28:19 -0300

    1/7 2

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
-
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelector('.parametro-senha__botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-    //tamanhoSenha = tamanhoSenha-1;
-    if (tamanhoSenha > 1){
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
         tamanhoSenha--;
-        numeroSenha.textContent = tamanhoSenha;
     }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
-function aumentaTamanho(){
-    if (tamanhoSenha > 20){
+function aumentaTamanho() {
+    if (tamanhoSenha < 20){
         tamanhoSenha++;
-        numeroSenha.textContent = tamanhoSenha;
     }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha() {
+    let senha = '' ;
+    for(let i=0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
 }
